bf2793eb512c9046de7b2039d9aa0015638eff9f
jly49 <70277955+jly49@users.noreply.github.com>
2021-12-02 16:56:36 -0800

    blank .h file

---
+++ Interface.h
@@ -0,0 +1,21 @@
+/*
+*	Connorpog!
+* 
+*	Hoang, Connor
+*	Dang, Jeffrey
+*	Ly, Jenny
+*	Nguyen, Catherine
+*	Quach, Bryan
+* 
+*	December 2, 2021
+* 
+*	CS A250
+*	Project 1
+*/
+
+#ifndef INTERFACE.H
+#define INTERFACE.H
+
+#include ...
+
+void processSelection
