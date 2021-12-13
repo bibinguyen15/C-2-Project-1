5794259100a121ad39faec3a40a9f1da0272c7b3
bibinguyen15 <78516882+bibinguyen15@users.noreply.github.com>
2021-12-13 13:07:09 -0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -28,39 +28,3 @@ int main()
     }
     return 0;
 }
-
-
-//int main()
-//{
-//    DonorList list1;
-//    list1.addDonor("huuuuuh", "whaaaa", 1000, 6136);
-//    list1.addDonor("huuuuu", "whaaa", 1001, 613);
-//    list1.addDonor("huuuu", "whaa", 1002, 61);
-//    list1.addDonor("huuu", "wha", 1003, 6);
-//
-//    DonorList list2;
-//    list2.addDonor("whaaaa", "huuuuuh", 2000, 3000);
-//    list2.addDonor("whaaa", "huuuuu", 2001, 300);
-//    //list2.addDonor("huuuu", "whaa", 2002, 61);
-//    //list2.addDonor("huuu", "wha", 2003, 6);
-//
-//    list1.printAllDonors();
-//    cout << endl;
-//    list2.printAllDonors();
-//    cout << endl;
-//    
-//    list1 = list2;
-//    cout << endl;
-//
-//    list1.printAllDonors();
-//    cout << endl;
-//    list2.printAllDonors();
-//    cout << endl;
-//    list1.clearList();
-//    cout << endl;
-//
-//    list1.printAllDonors();
-//    cout << endl;
-//    list2.printAllDonors();
-//    return 0;
-//}
